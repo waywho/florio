f30519e0af7d78951358a74e3dde070f3a463be3
waywho <walzerfan@yahoo.com>
2016-10-15 20:49:35 +0000

    change layout

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,16 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+
+require 'csv'
+Word.delete_all
+csv_text = File.read(Rails.root.join('lib', 'seeds', 'florio6.csv'))
+csv = CSV.parse(csv_text, :headers => true, :encoding => 'UTF-8')
+csv.each do |row|
+	# item = find_by_id(row["id"]) || new
+	item = Word.new
+	item.attributes = row.to_hash.slice(*Word.column_names)
+	item.save!
+end
+puts "There are now #{Word.count} rows in the transactions table"
