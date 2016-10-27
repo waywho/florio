0cbf7e3e4c1b0c66bc750d676c8095f8ba934adb
waywho <walzerfan@yahoo.com>
2016-10-27 03:50:09 +0000

    new text search

--- app/controllers/words_controller.rb
+++ app/controllers/words_controller.rb
@@ -13,6 +13,27 @@ class WordsController < ApplicationController
 		end
 	end
 
+	def text_search
+	end
+
+	def text_translate
+		@text = word_params[:text].split(/\s*[.,;?!]\s*|\s|[\r\n]+/x)
+		
+		@results = {}
+		@text.each do |search_word|
+			word = Word.find_by_fuzzy_word(search_word, :limit=> 1).first
+			if word.definition.nil?
+				@results[search_word] = search_word
+			# elsif word.word.nil?
+			# 	@results[search_word] = search_word
+			else
+				@results[word.word] = word.definition
+			end
+		end
+		respond_to :html, :js
+		# render json: {results: @results, text: @text}
+	end
+
 	def show
 		@word = Word.find(params[:id])
 		@words = Word.find_by_fuzzy_word(@word.word, :limit => 10)
@@ -33,6 +54,6 @@ class WordsController < ApplicationController
 	private
 
 	def word_params
-		params.require(:word).permit(:word, :definition, :alternative_word, :alternative_definition)
+		params.require(:word).permit(:word, :definition, :alternative_word, :alternative_definition, :text)
 	end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -7,6 +7,9 @@ Rails.application.routes.draw do
 
   resources :autosearches
 
+  get :text_search, to: 'words#text_search'
+  post :text_translate, to: 'words#text_translate'
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
